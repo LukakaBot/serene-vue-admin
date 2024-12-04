55d1896bb9fd07ace430806954274b159b643902
Lokaka <monishine_code@163.com>
2024-12-04 22:38:22 +0800

    feat: ✨ 主题配置弹窗增加水印内容配置

--- src/layouts/BaseLayout/components/BaseHeader/components/ConfigDrawer.vue
+++ src/layouts/BaseLayout/components/BaseHeader/components/ConfigDrawer.vue
@@ -1,22 +1,32 @@
 <template>
-  <n-drawer v-model:show="show">
+  <n-drawer v-model:show="show" :width="350">
     <n-drawer-content title="系统配置">
-      <n-divider> 主题风格 </n-divider>
-      <n-switch v-model:value="configStore.isDarkMode">
-        <template #checked-icon>
-          <component :is="renderIcon({ name: 'mage:moon' })" />
-        </template>
-        <template #unchecked-icon>
-          <component :is="renderIcon({ name: 'mage:sun' })" />
-        </template>
-        <template #checked> dark </template>
-        <template #unchecked> light </template>
-      </n-switch>
-      <n-divider> 界面配置 </n-divider>
-      <n-flex justify="space-between" align="center">
-        <label>是否显示水印</label>
-        <n-switch v-model:value="configStore.showWatermark" />
-      </n-flex>
+      <n-space vertical>
+        <n-divider> 主题风格 </n-divider>
+        <n-space justify="space-between" align="center">
+          <label>暗黑模式</label>
+          <n-switch v-model:value="configStore.isDarkMode">
+            <template #checked-icon>
+              <component :is="renderIcon({ name: 'mage:moon' })" />
+            </template>
+            <template #unchecked-icon>
+              <component :is="renderIcon({ name: 'mage:sun' })" />
+            </template>
+            <template #checked> dark </template>
+            <template #unchecked> light </template>
+          </n-switch>
+        </n-space>
+
+        <n-divider> 界面配置 </n-divider>
+        <n-space justify="space-between" align="center">
+          <label>是否显示水印</label>
+          <n-switch v-model:value="configStore.showWatermark" />
+        </n-space>
+        <n-space justify="space-between" align="center">
+          <label>水印内容</label>
+          <n-input v-model:value="configStore.watermarkContent" size="small" />
+        </n-space>
+      </n-space>
     </n-drawer-content>
   </n-drawer>
 </template>
