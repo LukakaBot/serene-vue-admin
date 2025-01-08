3e0b77a4b0a09000ba4a8a55210041d010f447f2
Lukaka <monishine_code@163.com>
2025-01-08 17:26:17 +0800

    feat: 优化BaseIcon组件

--- src/components/BaseIcon/index.vue
+++ src/components/BaseIcon/index.vue
@@ -1,50 +1,3 @@
-<!-- <script lang="ts">
-import { defineComponent } from 'vue';
-import { Icon } from '@iconify/vue';
-
-export default defineComponent({
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    color: {
-      type: String,
-      default: ''
-    },
-    size: {
-      type: Number,
-      default: 16
-    },
-  },
-  setup(props) {
-    const isLocalIcon = computed(() => props.name.startsWith('icon-'));
-
-    const iconName = computed(() => `#svg-${props.name}`);
-
-    const iconStyle = computed(() => ({
-      color: props.color,
-    }));
-
-    return () => {
-      if (isLocalIcon.value) {
-        return h('svg', { style: iconStyle.value, width: props.size, height: props.size, 'aria-hidden': 'true' }, [
-          h('use', { 'xlink:href': iconName.value, fill: props.color })
-        ]);
-      }
-
-      return h(Icon, { icon: props.name, width: props.size, height: props.size, color: props.color });
-    }
-  }
-})
-</script>
-
-<style scoped></style> -->
-
-<template>
-  <BaseIcon />
-</template>
-
 <script setup lang="tsx">
 import { Icon } from '@iconify/vue';
 
@@ -66,16 +19,13 @@ const iconStyle = computed(() => ({
   color: props.color,
 }));
 
-// 使用一个渲染函数
-const BaseIcon = () => {
-  if (isLocalIcon.value) {
-    return h('svg', { style: iconStyle.value, width: props.size, height: props.size, 'aria-hidden': 'true' }, [
-      h('use', { 'xlink:href': iconName.value, fill: props.color })
-    ]);
-  } else {
-    return h(Icon, { icon: props.name, width: props.size, height: props.size, color: props.color });
-  }
-};
+defineRender(() => (
+  isLocalIcon.value
+    ? <svg style={iconStyle.value} width={props.size} height={props.size} aria-hidden="true">
+      <use xlinkHref={iconName.value} fill={props.color} />
+    </svg>
+    : <Icon icon={props.name} width={props.size} height={props.size} color={props.color} />
+));
 </script>
 
 <style scoped></style>
