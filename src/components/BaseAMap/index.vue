c1d6c69865164c720a5d36e74f3ed89876789a14
鲨鱼辣椒 <monishine_code@163.com>
2025-06-05 15:28:09 +0800

    feat(comp): 调整BaseAMap

--- src/components/BaseAMap/index.vue
+++ src/components/BaseAMap/index.vue
@@ -1,36 +1,33 @@
 <script setup lang="tsx">
 import type { CSSProperties } from 'vue';
-import AMapLoader from "@amap/amap-jsapi-loader";
+import AMapLoader from '@amap/amap-jsapi-loader';
 
 type Props = {
-  /** 宽度 */
-  width?: string;
-  /** 高度 */
-  height?: string;
-  /** 中心纬度 */
-  latitude?: number;
-  /** 中心经度 */
-  longitude?: number;
-  /** 地图缩放层级 */
-  zoom?: number;
-  /** 是否显示高德地图logo */
-  showLogo?: boolean;
+	/** 宽度 */
+	width?: string;
+	/** 高度 */
+	height?: string;
+	/** 中心纬度 */
+	latitude?: number;
+	/** 中心经度 */
+	longitude?: number;
+	/** 地图缩放层级 */
+	zoom?: number;
+	/** 是否显示高德地图logo */
+	showLogo?: boolean;
 };
 
-const props = withDefaults(
-  defineProps<Props>(),
-  {
-    width: '100%',
-    height: '100%',
-    latitude: 39.90923,
-    longitude: 116.397428,
-    zoom: 13,
-    showLogo: true,
-  }
-);
+const props = withDefaults(defineProps<Props>(), {
+	width: '100%',
+	height: '100%',
+	latitude: 39.90923,
+	longitude: 116.397428,
+	zoom: 13,
+	showLogo: true,
+});
 
 type Emits = {
-  (event: 'complete', map: AMap.Map): void;
+	(event: 'complete', map: AMap.Map): void;
 };
 
 const emits = defineEmits<Emits>();
@@ -39,69 +36,68 @@ const emits = defineEmits<Emits>();
 const map = shallowRef<AMap.Map | null>(null);
 
 function initMap() {
-  window._AMapSecurityConfig = {
-    securityJsCode: import.meta.env.VITE_AMAP_SECURITY_JS_CODE,
-  };
+	window._AMapSecurityConfig = {
+		securityJsCode: import.meta.env.VITE_AMAP_SECURITY_JS_CODE,
+	};
 
-  const options = {
-    key: import.meta.env.VITE_AMAP_KEY,
-    version: "2.0",
-  };
+	const options = {
+		key: import.meta.env.VITE_APP_MAP_KEY,
+		version: '2.0',
+	};
 
-  AMapLoader.load(options)
-    .then((AMap) => {
-      window.AMap = AMap;
-      map.value = new AMap.Map('container', {
-        center: [props.longitude, props.latitude],
-        zoom: props.zoom,
-        canvas: {
-          willReadFrequently: true
-        }
-      });
-      map.value?.on('complete', function () {
-        emits('complete', map.value!);
-      });
-    }).catch((err) => {
-      console.log(err);
-    });
+	AMapLoader.load(options)
+		.then((AMap) => {
+			window.AMap = AMap;
+			map.value = new AMap.Map('container', {
+				center: [props.longitude, props.latitude],
+				zoom: props.zoom,
+				canvas: {
+					willReadFrequently: true,
+				},
+			});
+			map.value?.on('complete', function () {
+				emits('complete', map.value!);
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
 
 const mapContainerStyle = computed<CSSProperties>(() => ({
-  width: props.width,
-  height: props.height
+	width: props.width,
+	height: props.height,
 }));
 
 const hiddenMapLogoClassName = computed(() => ({
-  'amap-logo-hidden': !props.showLogo
+	'amap-logo-hidden': !props.showLogo,
 }));
 
 onMounted(() => initMap());
 
 onUnmounted(() => {
-  map.value?.destroy();
+	map.value?.destroy();
 });
 
 defineExpose({ map });
 
 defineRender(() => (
-  <div
-    id="container"
-    class={hiddenMapLogoClassName.value}
-    style={mapContainerStyle.value}
-  >
-  </div>
+	<div
+		id='container'
+		class={hiddenMapLogoClassName.value}
+		style={mapContainerStyle.value}
+	></div>
 ));
 </script>
 
 <style lang="scss" scoped>
 .amap-logo-hidden {
+	:deep(.amap-logo) {
+		opacity: 0;
+	}
 
-  :deep(.amap-logo) {
-    opacity: 0;
-  }
-
-  :deep(.amap-copyright) {
-    opacity: 0;
-  }
+	:deep(.amap-copyright) {
+		opacity: 0;
+	}
 }
 </style>
