0909d2eb79c7ec023c0297f7126cca40834e2636
鲨鱼辣椒 <monishine_code@163.com>
2025-06-06 13:43:56 +0800

    feat: 替换登录页背景

--- src/views/login/components/LoginBox1.vue
+++ src/views/login/components/LoginBox1.vue
@@ -1,14 +1,15 @@
 <script setup lang="tsx">
-import SvgImage from "./SvgImage.vue";
-import LoginForm from "./LoginForm.vue";
+// import SvgImage from './SvgImage.vue';
+import WaveSvg from './WaveSvg.vue';
+import LoginForm from './LoginForm.vue';
 
 defineRender(() => (
-  <div>
-    <SvgImage />
-    <div class="absolute top-50% left-50% -translate-50% rounded-3">
-      <LoginForm />
-    </div>
-  </div>
+	<div>
+		<WaveSvg />
+		<div class='absolute top-50% left-50% -translate-50% rounded-3'>
+			<LoginForm />
+		</div>
+	</div>
 ));
 </script>
 
