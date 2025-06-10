79844cea1bcf5fc920b5498435769eb52ea01ab1
鲨鱼辣椒 <monishine_code@163.com>
2025-06-10 13:57:34 +0800

    feat: tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import VueMacros from 'unplugin-vue-macros/vite';
+import VueMacros from 'vue-macros/vite';
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
@@ -12,7 +12,7 @@ import { viteMockServe } from 'vite-plugin-mock';
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig((({ command, mode }) => {
+export default defineConfig((({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
