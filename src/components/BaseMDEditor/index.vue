0052bbd54eeb7a274539efef53f2b564084da21d
鲨鱼辣椒 <monishine_code@163.com>
2025-07-04 14:02:43 +0800

    feat: 封装富文本编辑器

---
+++ src/components/BaseMDEditor/index.vue
@@ -0,0 +1,72 @@
+<script setup lang="tsx">
+import type { ToolbarNames } from 'md-editor-v3';
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+interface IProps {
+	value: string;
+	/** 是否展示预览 */
+	preview?: boolean;
+	/** 工具栏 */
+	toolbars?: ToolbarNames[];
+	'onUpdate:value'?: (value: string) => void;
+	onUpdateValue?: (value: string) => void;
+}
+
+const props = withDefaults(defineProps<IProps>(), {
+	preview: false,
+	toolbars: () => [
+		'bold',
+		'underline',
+		'italic',
+		'-',
+		'title',
+		'strikeThrough',
+		'sub',
+		'sup',
+		'quote',
+		'unorderedList',
+		'orderedList',
+		'task',
+		'-',
+		'codeRow',
+		'code',
+		'link',
+		'image',
+		'table',
+		'mermaid',
+		'katex',
+		'-',
+		'revoke',
+		'next',
+		'=',
+		'pageFullscreen',
+		'fullscreen',
+		'preview',
+		'previewOnly',
+		'htmlPreview',
+		'catalog',
+	],
+});
+
+const text = ref('');
+
+function handleUpdateValue(value: string) {
+	const { 'onUpdate:value': onUpdateValue, onUpdateValue: onUpdateValue2 } =
+		props;
+
+	onUpdateValue && onUpdateValue(value);
+	onUpdateValue2 && onUpdateValue2(value);
+}
+
+defineRender(() => (
+	<MdEditor
+		v-model={text}
+		toolbars={props.toolbars}
+		preview={props.preview}
+		onUpdate:modelValue={handleUpdateValue}
+	/>
+));
+</script>
+
+<style scoped></style>
