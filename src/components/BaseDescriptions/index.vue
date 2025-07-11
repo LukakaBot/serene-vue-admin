c5b2e2aeaa229184a5d1d8880ca15095b543f93d
鲨鱼辣椒 <monishine_code@163.com>
2025-07-11 13:48:03 +0800

    feat: 调整BaseDescriptions

--- src/components/BaseDescriptions/index.vue
+++ src/components/BaseDescriptions/index.vue
@@ -4,48 +4,41 @@ import type { BaseDescription, BaseDescriptionData } from './types';
 import type { DescriptionsProps } from 'naive-ui';
 
 interface Props extends DescriptionsProps {
-  /** 描述列表 */
-  list: BaseDescription[];
-  /** 数据 */
-  data: BaseDescriptionData;
-  /** 标签样式 */
-  itemLabelStyle?: CSSProperties;
-  /** 内容样式 */
-  itemContentStyle?: CSSProperties;
-};
+	/** 描述列表 */
+	list: BaseDescription[];
+	/** 数据 */
+	data: BaseDescriptionData;
+	/** 标签样式 */
+	itemLabelStyle?: CSSProperties;
+	/** 内容样式 */
+	itemContentStyle?: CSSProperties;
+}
 
-const props = withDefaults(
-  defineProps<Props>(),
-  {
-    list: () => [],
-    itemLabelStyle: () => ({}),
-    itemContentStyle: () => ({}),
-  }
-);
+const props = withDefaults(defineProps<Props>(), {
+	list: () => [],
+	itemLabelStyle: () => ({}),
+	itemContentStyle: () => ({}),
+});
 
 const attrs = useAttrs();
 
-defineRender(() => (
-  <n-descriptions
-    {...attrs}
-    bordered={true}
-  >
-    {props.list.map((item, index) => (
-      <n-descriptions-item
-        key={index}
-        label={item.text}
-        label-style={props.itemLabelStyle}
-        content-style={props.itemContentStyle}
-      >
-        {
-          item.render
-            ? item.render(props.data, item.field)
-            : props.data[item.field]
-        }
-      </n-descriptions-item>
-    ))}
-  </n-descriptions>
-));
+defineRender(() => {
+	const { list, data, itemContentStyle, itemLabelStyle } = props;
+	return (
+		<n-descriptions {...attrs} bordered={true}>
+			{list.map((item, index) => (
+				<n-descriptions-item
+					key={index}
+					label={item.text}
+					labelStyle={itemLabelStyle}
+					contentStyle={itemContentStyle}
+				>
+					{item.render ? item.render(data, item.field) : data[item.field]}
+				</n-descriptions-item>
+			))}
+		</n-descriptions>
+	);
+});
 </script>
 
 <style scoped></style>
