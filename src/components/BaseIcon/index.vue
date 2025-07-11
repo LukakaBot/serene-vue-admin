b8ec4f869d9ec9c1741a49a0acf9a1efffc6a661
鲨鱼辣椒 <monishine_code@163.com>
2025-07-11 11:30:58 +0800

    feat: BaseIcon组件color属性增加默认值

--- src/components/BaseIcon/index.vue
+++ src/components/BaseIcon/index.vue
@@ -2,33 +2,43 @@
 import { Icon } from '@iconify/vue';
 
 type Props = {
-  name: string,
-  color?: string,
-  size?: number,
+	name: string;
+	color?: string;
+	size?: number;
 };
 
-const props = withDefaults(
-  defineProps<Props>(),
-  {
-    size: 16,
-  }
-);
+const props = withDefaults(defineProps<Props>(), {
+	color: 'currentColor',
+	size: 16,
+});
 
 const isLocalIcon = computed(() => props.name.startsWith('icon-'));
 
 const iconName = computed(() => `#svg-${props.name}`);
 
 const iconStyle = computed(() => ({
-  color: props.color,
+	color: props.color,
 }));
 
-defineRender(() => (
-  isLocalIcon.value
-    ? <svg style={iconStyle.value} width={props.size} height={props.size} aria-hidden="true">
-      <use xlinkHref={iconName.value} fill={props.color} />
-    </svg>
-    : <Icon icon={props.name} width={props.size} height={props.size} color={props.color} />
-));
+defineRender(() =>
+	isLocalIcon.value ? (
+		<svg
+			style={iconStyle.value}
+			width={props.size}
+			height={props.size}
+			aria-hidden='true'
+		>
+			<use xlinkHref={iconName.value} fill={props.color} />
+		</svg>
+	) : (
+		<Icon
+			icon={props.name}
+			width={props.size}
+			height={props.size}
+			color={props.color}
+		/>
+	)
+);
 </script>
 
 <style scoped></style>
