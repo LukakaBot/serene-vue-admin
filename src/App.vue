fc5da680b2746852a7c12e116e47bf7abc2a2ff2
鲨鱼辣椒 <monishine_code@163.com>
2025-07-11 16:32:27 +0800

    feat: App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="tsx">
-import { RouterView } from "vue-router";
-import { darkTheme, zhCN, dateZhCN, NConfigProvider } from "naive-ui";
-import { useConfigStore, useThemeStore } from "./store";
-import BaseProvider from "@/components/BaseProvider/index.vue";
+import { RouterView } from 'vue-router';
+import { darkTheme, zhCN, dateZhCN, NConfigProvider } from 'naive-ui';
+import { useConfigStore, useThemeStore } from './store';
+import BaseProvider from '@/components/BaseProvider/index.vue';
 
 const configStore = useConfigStore();
 
@@ -11,16 +11,16 @@ const themeStore = useThemeStore();
 const theme = computed(() => (configStore.isDarkMode ? darkTheme : undefined));
 
 defineRender(() => (
-  <NConfigProvider
-    theme={theme.value}
-    theme-overrides={themeStore.appTheme}
-    locale={zhCN}
-    date-locale={dateZhCN}
-  >
-    <BaseProvider>
-      <RouterView />
-    </BaseProvider>
-  </NConfigProvider>
+	<NConfigProvider
+		theme={theme.value}
+		themeOverrides={themeStore.appTheme}
+		locale={zhCN}
+		dateLocale={dateZhCN}
+	>
+		<BaseProvider>
+			<RouterView />
+		</BaseProvider>
+	</NConfigProvider>
 ));
 </script>
 
