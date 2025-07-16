dfaa50d00cd7cb9cecc0e54aa9ace646b012f306
鲨鱼辣椒 <monishine_code@163.com>
2025-07-16 14:50:47 +0800

    refactor(store): 调整useConfigStore的类型

--- src/store/modules/config/types.ts
+++ src/store/modules/config/types.ts
@@ -1,13 +1,21 @@
 import type { ThemeState } from "../theme/types";
 
-export type ConfigState = {
+export interface ConfigState {
   /** ----- 水印 ----- */
-  /** 是否显示水印 */
+  /** 
+   * 是否显示水印
+   */
   showWatermark: boolean;
-  /** 水印内容 */
+  /** 
+   * 水印内容
+   */
   watermarkContent: string;
-  /** 暗黑模式 */
+  /** 
+   * 暗黑模式
+   */
   isDarkMode: boolean;
-  /** 主题 */
+  /** 
+   * 主题
+   */
   theme: ThemeState;
 }
