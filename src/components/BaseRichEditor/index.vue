2c83968be763d19aba9578fe39de90efe056e899
鲨鱼辣椒 <monishine_code@163.com>
2025-07-16 11:17:29 +0800

    refactor(comp): 调整BaseRichEditor组件的类型

--- src/components/BaseRichEditor/index.vue
+++ src/components/BaseRichEditor/index.vue
@@ -1,45 +1,12 @@
 <script setup lang="tsx">
 import type { CSSProperties } from 'vue';
 import '@wangeditor/editor/dist/css/style.css';
-import type {
-	IDomEditor,
-	IEditorConfig,
-	IToolbarConfig,
-} from '@wangeditor/editor';
+import type { IDomEditor } from '@wangeditor/editor';
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
 import { isNumber } from '@/utils/is';
+import type { BaseRichEditorProps } from './types';
 
-interface Props {
-	value: string;
-	width: string | number;
-	height: string | number;
-	borderColor?: string;
-	/** body 样式 */
-	bodyStyle?: CSSProperties;
-	/** 编辑器样式 */
-	editorStyle?: CSSProperties;
-	mode: 'simple' | 'default';
-	/** 工具栏配置 */
-	toolbarConfig: Partial<IToolbarConfig>;
-	/** 编辑器配置 */
-	editorConfig: Partial<IEditorConfig>;
-	/** 编辑器内容变化时触发 */
-	'onUpdate:value'?: (value: string) => void;
-	/** 编辑器内容变化时触发 */
-	onUpdateValue?: (value: string) => void;
-	/** 编辑器创建完毕时触发 */
-	onCreated?: (editor: IDomEditor) => void;
-	/** 编辑器内容、选区变化时触发 */
-	onChange?: (editor: IDomEditor) => void;
-	/** 编辑器销毁时触发 */
-	onDestroyed?: () => void;
-	/** 编辑器聚焦时触发 */
-	onFocus?: (editor: IDomEditor) => void;
-	/** 编辑器失焦时触发 */
-	onBlur?: (editor: IDomEditor) => void;
-}
-
-const props = withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<BaseRichEditorProps>(), {
 	width: '100%',
 	height: '100%',
 	borderColor: '#e0e0e6',
