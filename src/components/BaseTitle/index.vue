2354b655935f4073505cc64fbd7b6de7f51a3a76
鲨鱼辣椒 <monishine_code@163.com>
2025-07-16 13:40:19 +0800

    refactor(comp): 调整BaseTitle组件的类型

--- src/components/BaseTitle/index.vue
+++ src/components/BaseTitle/index.vue
@@ -1,32 +1,23 @@
 <script setup lang="tsx">
-import type { CSSProperties } from "vue";
+import type { BaseTitleProps } from './types';
 
-type Props = {
-  /** 标题 */
-  title: string;
-  /** 标题样式 */
-  titleStyle?: CSSProperties;
-  /** 是否显示 tag */
-  showTag?: boolean;
-  /** tag 方向 */
-  tagDirection?: "left" | "bottom";
-};
-
-const props = withDefaults(defineProps<Props>(), {
-  title: "我是标题",
-  titleStyle: () => ({}),
-  showTag: true,
-  tagDirection: "bottom",
+const props = withDefaults(defineProps<BaseTitleProps>(), {
+	title: '我是标题',
+	titleStyle: () => ({}),
+	showTag: true,
+	tagDirection: 'bottom',
 });
 
 const tagClassName = computed(() =>
-  props.tagDirection === "left" ? "left-tag-title" : "bottom-tag-title"
+	props.showTag && props.tagDirection === 'left'
+		? 'left-tag-title'
+		: 'bottom-tag-title'
 );
 
 defineRender(() => (
-  <div class={tagClassName.value} style={props.titleStyle}>
-    {props.title}
-  </div>
+	<div class={tagClassName.value} style={props.titleStyle}>
+		{props.title}
+	</div>
 ));
 </script>
 
---
+++ src/components/BaseTitle/types.ts
@@ -0,0 +1,20 @@
+import type { CSSProperties } from "vue";
+
+export interface BaseTitleProps {
+  /** 
+   * 标题
+   */
+  title: string;
+  /** 
+   * 标题样式
+   */
+  titleStyle?: CSSProperties;
+  /** 
+   * 是否显示 tag
+   */
+  showTag?: boolean;
+  /** 
+   * tag 方向
+   */
+  tagDirection?: "left" | "bottom";
+};
