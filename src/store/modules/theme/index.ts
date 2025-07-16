404dfe9d432b6808c41e991c0d57fcf988847351
鲨鱼辣椒 <monishine_code@163.com>
2025-07-16 15:24:59 +0800

    refactor(store): 调整useThemeStore的类型

--- src/store/modules/theme/index.ts
+++ src/store/modules/theme/index.ts
@@ -1,24 +1,11 @@
 import { defineStore } from 'pinia';
 import type { GlobalThemeOverrides } from 'naive-ui';
 import type { ThemeState } from './types';
-// import { lighten } from '@/utils/tools';
 
 export const useThemeStore = defineStore('theme', {
     state: (): ThemeState => {
-        // const appTheme = '#2d8cf0';
-        // const lightenStr = lighten(appTheme, 6);
-
         return {
             appTheme: {
-                // common: {
-                //     // primaryColor: appTheme,
-                //     // primaryColorHover: lightenStr,
-                //     // primaryColorPressed: lightenStr,
-                //     // primaryColorSuppl: appTheme,
-                // },
-                // LoadingBar: {
-                //     // colorLoading: appTheme,
-                // },
                 Scrollbar: {
                     width: '8px',
                     height: '8px',
--- src/store/modules/theme/types.ts
+++ src/store/modules/theme/types.ts
@@ -1,6 +1,6 @@
 import type { GlobalThemeOverrides } from 'naive-ui';
 
-export type ThemeState = {
+export interface ThemeState {
     /** 应用主题风格 */
     appTheme: GlobalThemeOverrides;
 };
