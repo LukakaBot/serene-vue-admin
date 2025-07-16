f9dbc8d0c2a4b7c8c7b8cd33dec7f683f71286df
鲨鱼辣椒 <monishine_code@163.com>
2025-07-16 11:35:33 +0800

    refactor(comp): 调整BaseDescriptions组件的类型

--- src/components/BaseDescriptions/types.ts
+++ src/components/BaseDescriptions/types.ts
@@ -1,15 +1,13 @@
 import type { VNodeChild } from 'vue';
 import type { InternalRowData } from 'naive-ui/es/data-table/src/interface';
 
-export type BaseDescriptionRender = (data: BaseDescriptionData, field: string) => VNodeChild;
+export type DescriptionData<T = InternalRowData> = T;
 
-export type BaseDescription = {
+export interface BaseDescription {
   /** 描述文本 */
   text: string;
   /** 描述字段 */
   field: string;
   /** 渲染函数 */
-  render?: BaseDescriptionRender;
+  render?: (data: DescriptionData, field: string) => VNodeChild;
 };
-
-export type BaseDescriptionData<T = InternalRowData> = T;
