33b63af60532199a77bc8ad2b09f4053421ed2a3
鲨鱼辣椒 <monishine_code@163.com>
2025-09-13 12:59:09 +0800

    chore: 添加macros的TypeScript 支持

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
 import antfu from '@antfu/eslint-config'
+import vueMacros from '@vue-macros/eslint-config'
 
 export default antfu({
   formatters: true,
   unocss: true,
   vue: true,
+},
+{
+  rules:{
+    ...vueMacros.rules,
+  }
 })
--- src/components/BaseTitle/index.vue
+++ src/components/BaseTitle/index.vue
@@ -1,24 +1,24 @@
 <script setup lang="tsx">
-import type { BaseTitleProps } from './types';
+import type { BaseTitleProps } from './types'
 
 const props = withDefaults(defineProps<BaseTitleProps>(), {
-	title: '我是标题',
-	titleStyle: () => ({}),
-	showTag: true,
-	tagDirection: 'bottom',
-});
+  title: '我是标题',
+  titleStyle: () => ({}),
+  showTag: true,
+  tagDirection: 'bottom',
+})
 
 const tagClassName = computed(() =>
-	props.showTag && props.tagDirection === 'left'
-		? 'left-tag-title'
-		: 'bottom-tag-title'
-);
+  props.showTag && props.tagDirection === 'left'
+    ? 'left-tag-title'
+    : 'bottom-tag-title',
+)
 
 defineRender(() => (
-	<div class={tagClassName.value} style={props.titleStyle}>
-		{props.title}
-	</div>
-));
+  <div class={tagClassName.value} style={props.titleStyle}>
+    {props.title}
+  </div>
+))
 </script>
 
 <style scoped></style>
