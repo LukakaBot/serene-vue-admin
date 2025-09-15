19fa16fa81c8dd07c2920c7cd83e5699830baec6
鲨鱼辣椒 <monishine_code@163.com>
2025-09-15 09:59:55 +0800

    style: 代码格式修改

--- src/directives/auth/index.ts
+++ src/directives/auth/index.ts
@@ -5,16 +5,18 @@ export const auth: ObjectDirective = {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
     const { value }: { value: string } = binding;
 
-    if (!value)
+    if (!value) {
       throw new Error(
         "[Directive/auth]: Authorization value is missing or empty.",
       );
+    }
 
     const { auths } = useRouteStore().currentRoute.meta as AppRoute.Meta;
-    if (!auths)
+    if (!auths) {
       throw new Error(
         '[Directive/auth]: "auths" field is missing in current route meta.',
       );
+    }
     const hasAuth = auths.some((auth) => auth.name === value);
 
     if (!hasAuth) {
